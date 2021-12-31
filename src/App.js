2ea47062e7129189e2cda4a6585aa830a529b576
Joe Owen <joe.a.owen@gmail.com>
2021-12-30 18:33:24 -0800

    Intro page with scrolling animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import pfp from './images/profile-picture-header.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrapper">
+      <div id="intro" className="intro">
+        <img src={pfp} alt="intro" />
+        <h1>Hey, I'm Joe.</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome! This website is a place where I store all my creative work. I take pictures, make videos, and do lots of other things. Feel free to take a look around and hopefully you'll like what you see.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+        Always happy to work with people, so if you're interested in partering or have any questions, you can find my email at the bottom of this page.
+        </p>
+        <p className="scroll-to-view">Scroll to view more</p>
+      </div>
+      <div className="container">
+        <div id="header" className="header">
+          <img id="header-img" className="header-img hidden" src={pfp} alt="profile" />
+          <h1 id="header-text" className="header-text hidden">Joe Owen<span>.ca</span></h1>
+        </div>
+        <div id="content" className="content">
+
+        </div>
+        <div className="footer">
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +10,39 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.body.onscroll = function() {ScrollTransition()}
+
+function ScrollTransition() {
+  const header = document.getElementById('header')
+  const intro = document.getElementById('intro')
+  const content = document.getElementById('content')
+  const headerImg = document.getElementById('header-img')
+  const headerText = document.getElementById('header-text')
+
+  let scrollPos = document.documentElement.scrollTop || document.body.scrollTop
+  
+  const ceiling = 80
+  const floor = 300
+
+  // Calculate current (Scroll Position / Heigh) for background alpha
+  let height = (intro.offsetHeight / 2)
+  let alpha = ((scrollPos) / height)
+
+  // Update header background color
+  content.style.backgroundColor = "rgba(26, 26, 26, " + alpha + ")"
+
+  // Hide header image
+  if (alpha >= 1) {
+    headerImg.classList.remove('hidden')
+    headerText.classList.remove('hidden')
+    header.style.backgroundColor = "rgba(26, 26, 26, 1)"
+  } else {
+    headerImg.classList.add('hidden')
+    headerText.classList.add('hidden')
+    header.style.backgroundColor = "rgba(26, 26, 26, 0)"
+  }
+
+
+  console.log(alpha)
+
+}
