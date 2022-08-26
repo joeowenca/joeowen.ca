12951fb761237e843138637accb67b981ef8d374
Joe Owen <joe.a.owen@gmail.com>
2022-08-26 15:13:53 -0700

    Fancy grid layout

--- src/PhotographyGallery.js
+++ src/PhotographyGallery.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react'
 import sportsImages from './images/Sports/index.js'
 
+const sportsImagesObject = Object.values(sportsImages)
+
 let PhotographyGallery = (props) => {
     return (
         <>
             <div className="photography-gallery">
                 <div className="automotive-gallery gallery">
+                    <div className="column">
                         {
-                            Object.values(sportsImages).map((value, index) => {
-                                return (
+                            sportsImagesObject.map((value, index) => {
+                                if ((index) % 2 === 0) {
+                                    return (
                                         <img src={value.default} onClick={() => props.showLightbox(index)} className="thumbnail" key={index} alt="" />
-                                )
+                                    )
+                                }
                             })
                         }
+                    </div>
+
+                    <div className="column">
+                        {
+                            sportsImagesObject.map((value, index) => {
+                                if ((index + 1) % 2 === 0) {
+                                    return (
+                                        <img src={value.default} onClick={() => props.showLightbox(index)} className="thumbnail" key={index} alt="" />
+                                    )
+                                }
+                            })
+                        }
+                    </div>
                     
                 </div>
 
