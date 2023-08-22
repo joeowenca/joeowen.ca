ad194d4536b0a629b54f8a92c7a7fc80dd8e0cd3
Joe Owen <joe.a.owen@gmail.com>
2023-08-22 13:23:50 -0700

    Add header component

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import profilePicture from "../../public/profile picture.jpg";
+
+export default function Header() {
+  return (
+    <div className="fixed flex items-center w-screen max-h-20 p-3">
+      <div className="flex items-center">
+        <Image
+          src={profilePicture}
+          alt="Profile picture"
+          className="rounded-full w-14"
+        />
+        <h1 className="pl-3 font-serif text-2xl">Joe Owen</h1>
+      </div>
+    </div>
+  )
+}
