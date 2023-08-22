91afa21992c5216ce9fbdcf88d0842e6265920cc
Joe Owen <joe.a.owen@gmail.com>
2023-08-22 13:30:18 -0700

    Add header component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
+import Header from "@/components/header";
+
 export default function Home() {
   return (
-    <main className="p-5 pt-20">
-      <div className="text-center p-5">
-        <p className="pb-5">
-        Welcome! 
-        <br /> This website is a place for me to share all of my creative work. 
-        <br />I'm a photographer, video creator, musician, and software developer. 
-        <br />Feel free to take a look around and I hope you'll like what you see.
-        </p>
+    <>
+      <Header />
+      <main className="p-5 pt-20">
+        <div className="text-center p-5">
+          <p className="pb-5">
+          Welcome! 
+          <br /> This website is a place for me to share all of my creative work. 
+          <br />I'm a photographer, video creator, musician, and software developer. 
+          <br />Feel free to take a look around and I hope you'll like what you see.
+          </p>
 
-        <p>
-        Always happy to work with people. 
-        <br /> If you're interested in partering or have any questions, 
-        <br /> you can find my email at the bottom of this page.
-        </p>
-      </div>
-    </main>
+          <p>
+          Always happy to work with people. 
+          <br /> If you're interested in partering or have any questions, 
+          <br /> you can find my email at the bottom of this page.
+          </p>
+        </div>
+      </main>
+    </>
   )
 }
