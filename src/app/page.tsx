2abc6183bbe6474ecb1b44c5c6756c2726f17fd3
Joe Owen <joe.a.owen@gmail.com>
2023-08-21 18:41:35 -0700

    Add intro text to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        joeowen.ca
+    <main className="p-5 pt-20">
+      <div className="text-center p-5">
+        <p className="pb-5">
+        Welcome! 
+        <br /> This website is a place for me to share all of my creative work. 
+        <br />I'm a photographer, video creator, musician, and software developer. 
+        <br />Feel free to take a look around and I hope you'll like what you see.
+        </p>
+
+        <p>
+        Always happy to work with people. 
+        <br /> If you're interested in partering or have any questions, 
+        <br /> you can find my email at the bottom of this page.
+        </p>
       </div>
     </main>
   )
