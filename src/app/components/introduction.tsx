bce3dae2a9d305c477f4ef853c99982ad8553951
Joe Owen <joe.a.owen@gmail.com>
2023-08-22 18:43:57 -0700

    Create introduction component

---
+++ src/app/components/introduction.tsx
@@ -0,0 +1,26 @@
+export default function Introduction() {
+  const introTextSummary = `
+    Welcome!
+    This website is a place for me to share all of my creative work.
+    I'm a photographer, video creator, musician, and software developer.
+    Feel free to take a look around and I hope you'll like what you see.
+  `;
+
+  const introTextCollaborate = `
+    Always happy to work with people.
+    If you're interested in partering or have any questions,
+    you can find my email at the bottom of this page.
+  `;
+
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center outline outline-1">
+      <div className="p-5">
+        <h2 className="text-center font-serif text-2xl">Joe Owen</h2>
+        <div className="max-w-prose p-5">
+          <pre className="pb-5 font-sans whitespace-normal">{introTextSummary}</pre>
+          <pre className="font-sans whitespace-normal">{introTextCollaborate}</pre>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
+import Introduction from "@/app/components/introduction";
+
 export default function Home() {
-  const introTextSummary = `
-    Welcome!
-    This website is a place for me to share all of my creative work.
-    I'm a photographer, video creator, musician, and software developer.
-    Feel free to take a look around and I hope you'll like what you see.
-  `;
-
-  const introTextCollaborate = `
-    Always happy to work with people.
-    If you're interested in partering or have any questions,
-    you can find my email at the bottom of this page.
-  `;
-
   return (
-    <main className=" flex justify-center p-5 pt-20">
-      <div className="p-5">
-        <pre className="pb-5 font-sans">{introTextSummary}</pre>
-        <pre className="pb-5 font-sans">{introTextCollaborate}</pre>
-      </div>
+    <main>
+      <Introduction />
     </main>
   )
 }
