1bc93908921ea3b750740b6b138c9be4d8711bd7
Joe Owen <joe.owen@checkfront.com>
2023-08-23 10:36:49 -0700

    Improve showcase text responsiveness

--- src/app/components/showcase.tsx
+++ src/app/components/showcase.tsx
@@ -2,28 +2,30 @@ import React, { FC } from 'react';
 
 export default function Showcase() {
   return (
-    <div className="p-5 mt-[100vh] bg-black">
-      <h1 className="p-3 text-center font-serif text-2xl">{"Photography"}</h1>
-      <ShowcaseItem title="Sports">
-        This will become a description for sports photography
-      </ShowcaseItem>
-      <ShowcaseItem title="Automotive">
-        This will become a description for automotive photography
-      </ShowcaseItem>
-      <ShowcaseItem title="Landscapes">
-        This will become a description for landscape photography
-      </ShowcaseItem>
+    <div className="flex justify-center p-5 mt-[100vh] bg-black">
+      <div className="max-w-prose">
+        <h1 className="p-3 text-center font-serif text-2xl">{"Photography"}</h1>
+        <ShowcaseItem title="Sports">
+          This will become a description for sports photography
+        </ShowcaseItem>
+        <ShowcaseItem title="Automotive">
+          This will become a description for automotive photography
+        </ShowcaseItem>
+        <ShowcaseItem title="Landscapes">
+          This will become a description for landscape photography
+        </ShowcaseItem>
 
-      <h1 className="p-3 text-center font-serif text-2xl">{"Videos"}</h1>
-      <ShowcaseItem title="Mountain biking">
-        This will become a description for mountain bike videos
-      </ShowcaseItem>
-      <ShowcaseItem title="Automotive">
-        This will become a description for automotive videos
-      </ShowcaseItem>
-      <ShowcaseItem title="Skiing">
-        This will become a description for skiing videos
-      </ShowcaseItem>
+        <h1 className="p-3 text-center font-serif text-2xl">{"Videos"}</h1>
+        <ShowcaseItem title="Mountain biking">
+          This will become a description for mountain bike videos
+        </ShowcaseItem>
+        <ShowcaseItem title="Automotive">
+          This will become a description for automotive videos
+        </ShowcaseItem>
+        <ShowcaseItem title="Skiing">
+          This will become a description for skiing videos
+        </ShowcaseItem>
+      </div>
     </div>
   )
 }
@@ -37,7 +39,7 @@ function ShowcaseItem({ title, children }: ShowcaseItemProps) {
   return (
     <section className="pb-3">
       <h2 className="font-serif text-xl">{title}</h2>
-      <pre className="font-sans">{children}</pre>
+      <pre className="font-sans whitespace-normal">{children}</pre>
     </section>
   )
 }
