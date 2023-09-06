fc4cd60353455a80c8d10dc6c8f0d2cb173063d2
Joe Owen <joe.a.owen@gmail.com>
2023-09-06 16:20:20 -0700

    Change header text color based on scroll position

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,27 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import profilePicture from '../../public/profile-picture.jpg';
+import calculateScrollAlpha from '@/scripts/calculateScrollAlpha';
 
 export default function Header() {
+	const [color, setColor] = useState(0);
+
+	function handleScroll() {
+		setColor(calculateScrollAlpha() * 255);
+	}
+
+	useEffect(() => {
+		setColor(calculateScrollAlpha() * 255);
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
 		<div className="fixed top-0 left-0 flex items-center w-screen max-h-20 p-3 bg-black z-50">
 			<div className="flex items-center">
@@ -10,7 +30,12 @@ export default function Header() {
 					alt="Profile picture"
 					className="rounded-full w-14"
 				/>
-				<h1 className="pl-3 font-serif text-2xl">Joe Owen</h1>
+				<h1
+					className="pl-3 font-serif text-2xl"
+					style={{ color: `rgb(${color}, ${color}, ${color})` }}
+				>
+					Joe Owen
+				</h1>
 			</div>
 		</div>
 	);
