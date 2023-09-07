864337897d0cb19911ad93265ce06e787d743598
Joe Owen <joe.a.owen@gmail.com>
2023-09-07 11:39:16 -0700

    Add navigation links

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type NavigationProps = {
 	color: number;
 };
@@ -9,9 +11,28 @@ export default function Navigation({ color }: NavigationProps) {
 				className="flex items-center w-1/2 ml-auto"
 				style={{ color: `rgb(${color}, ${color}, ${color})` }}
 			>
-				<div className="-translate-x-1/2">Navigation items here</div>
+				<div className="flex -translate-x-1/2">
+					<NavLink page="/">Home</NavLink>
+					<NavLink page="photography">Photography</NavLink>
+					<NavLink page="videos">Videos</NavLink>
+					<NavLink page="music">Music</NavLink>
+					<NavLink page="projects">Projects</NavLink>
+				</div>
 				<div className="ml-auto">Socials links</div>
 			</div>
 		</>
+	);
+}
+
+type NavLinkProps = {
+	children: string;
+	page: string;
+};
+
+function NavLink({ children, page }: NavLinkProps) {
+	return (
+		<div className="p-2 text-lg">
+			<Link href={page}>{children}</Link>
+		</div>
 	);
 }
