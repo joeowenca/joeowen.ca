2efb9831a4d6ddbf92a12396b3f53921b0a2303c
Joe Owen <joe.a.owen@gmail.com>
2023-09-08 14:45:12 -0700

    Update photography showcase content

--- src/app/components/showcase.tsx
+++ src/app/components/showcase.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Image, { StaticImageData } from 'next/image';
+import sportsShowcase from '../../../public/sports-showcase.jpg';
+import landscapeShowcase from '../../../public/landscape-showcase.jpg';
+import automotiveShowcase from '../../../public/automotive-showcase.jpg';
 import calculateScrollAlpha from '@/scripts/calculateScrollAlpha';
 
 export default function Showcase() {
@@ -27,31 +31,50 @@ export default function Showcase() {
 			}`}
 			style={{ backgroundColor: `rgba(0, 0, 0, ${alpha})` }}
 		>
-			<div className="flex-1 max-w-prose">
+			<div>
 				<h1 className="p-3 text-center font-serif text-2xl">
 					{'Photography'}
 				</h1>
-				<ShowcaseItem title="Sports">
-					This will become a description for sports photography
+				<ShowcaseItem
+					title="Sports"
+					image={sportsShowcase}
+					align="left"
+				>
+					I'm all about snapping shots of extreme sports like mountain
+					biking and skiing because they get my heart racing just as
+					much as the athletes. I'm not just a photographer – I'm out
+					there in the action, vibing with fellow adventure junkies.
+					What really gets me stoked is catching those epic,
+					gravity-defying moments that make these sports a wild ride.
+					Flip through my pics to join in on the excitement and
+					adrenaline of extreme sports.
 				</ShowcaseItem>
-				<ShowcaseItem title="Automotive">
-					This will become a description for automotive photography
+				<ShowcaseItem
+					title="Landscapes"
+					image={landscapeShowcase}
+					align="right"
+				>
+					Landscape photography is my jam because it gets me out in
+					the great outdoors, exploring the beauty of our world.
+					Honestly, landscapes just look incredible, and I can't
+					resist framing those stunning vistas. Whether it's chasing
+					sunsets, hiking through mountains, or simply soaking in the
+					serenity of nature, I'm all about capturing these
+					breathtaking scenes and sharing their beauty with the world.
 				</ShowcaseItem>
-				<ShowcaseItem title="Landscapes">
-					This will become a description for landscape photography
-				</ShowcaseItem>
-
-				<h1 className="p-3 text-center font-serif text-2xl">
-					{'Videos'}
-				</h1>
-				<ShowcaseItem title="Mountain biking">
-					This will become a description for mountain bike videos
-				</ShowcaseItem>
-				<ShowcaseItem title="Automotive">
-					This will become a description for automotive videos
-				</ShowcaseItem>
-				<ShowcaseItem title="Skiing">
-					This will become a description for skiing videos
+				<ShowcaseItem
+					title="Automotive"
+					image={automotiveShowcase}
+					align="left"
+				>
+					I'm all about automotive photography because it's a ticket
+					to an awesome community of people and some seriously cool
+					rides. There's just something about capturing the unique
+					character of cars that gets me going. Plus, I love helping
+					folks preserve their pride and joy through my lens. So,
+					whether it's meeting fellow car enthusiasts, photographing
+					rare beauties, or immortalizing the essence of a car, I'm
+					revved up and ready to roll.
 				</ShowcaseItem>
 			</div>
 		</div>
@@ -60,14 +83,25 @@ export default function Showcase() {
 
 type ShowcaseItemProps = {
 	title: string;
+	image: StaticImageData;
+	align: string;
 	children: string;
 };
 
-function ShowcaseItem({ title, children }: ShowcaseItemProps) {
+function ShowcaseItem({ title, image, align, children }: ShowcaseItemProps) {
 	return (
-		<section className="p-2 pb-3">
-			<h2 className="font-serif text-xl">{title}</h2>
-			<pre className="font-sans whitespace-normal">{children}</pre>
+		<section className="flex flex-col lg:flex-row items-center p-2.5 pb-3 max-w-7xl">
+			<Image
+				className={`max-w-[65%] lg:max-w-sm lg:pb-0 pb-1  ${
+					align === 'right' ? 'lg:order-last lg:pl-5' : 'lg:pr-5'
+				}`}
+				src={image}
+				alt={`${title} showcase image`}
+			/>
+			<div className="max-w-prose">
+				<h2 className="font-serif text-2xl py-3">{title}</h2>
+				<pre className="font-sans whitespace-normal">{children}</pre>
+			</div>
 		</section>
 	);
 }
--- src/scripts/calculateScrollAlpha.ts
+++ src/scripts/calculateScrollAlpha.ts
@@ -1,5 +1,5 @@
 export default function calculateScrollAlpha():number {
-  const threshold = 3;
+  const threshold = 1.5;
   const scrollAlpha = Math.min(
     window.scrollY / (window.innerHeight / threshold),
     1,
