a5e8f3b29ecd0c17677c9272751dd062a026d0ad
Joe Owen <joe.a.owen@gmail.com>
2023-09-10 12:41:10 -0700

    Add content component to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,22 +1,16 @@
 import Header from '@/components/header';
+import Content from '@/components/content';
 import Footer from '@/components/footer';
 
 export default function Projects() {
 	return (
 		<>
 			<Header page="projects" />
-			<main className="flex flex-1 justify-center p-3 mt-24">
-				<div className="flex-1 max-w-prose">
-					<h1 className="text-center font-serif text-2xl">
-						Projects
-					</h1>
-					<section className="p-2 pb-3">
-						<pre className="font-sans whitespace-normal">
-							This is the Projects page.
-						</pre>
-					</section>
-				</div>
-			</main>
+			<Content title="Projects">
+				<pre className="font-sans whitespace-normal max-w-prose">
+					This is the projects page.
+				</pre>
+			</Content>
 			<Footer />
 		</>
 	);
