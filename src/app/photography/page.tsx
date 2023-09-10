6520f5bf40bbba26e0992fd86ca2ffef90e4aa67
Joe Owen <joe.a.owen@gmail.com>
2023-09-10 12:38:24 -0700

    Add content component to photography page

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,22 +1,16 @@
 import Header from '@/components/header';
+import Content from '@/components/content';
 import Footer from '@/components/footer';
 
 export default function Photography() {
 	return (
 		<>
 			<Header page="photography" />
-			<main className="flex flex-1 justify-center p-3 mt-24">
-				<div className="flex-1 max-w-prose">
-					<h1 className="text-center font-serif text-2xl">
-						Photography
-					</h1>
-					<section className="p-2 pb-3">
-						<pre className="font-sans whitespace-normal">
-							This is the Photography page.
-						</pre>
-					</section>
-				</div>
-			</main>
+			<Content title="Photography">
+				<pre className="font-sans whitespace-normal max-w-prose">
+					This is the photography page.
+				</pre>
+			</Content>
 			<Footer />
 		</>
 	);
