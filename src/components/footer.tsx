1bce66f9766633935f2a75e8da0eb99c33ea10d0
Joe Owen <joe.a.owen@gmail.com>
2023-09-09 19:22:41 -0700

    Add footer page links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,40 +1,145 @@
 export default function Footer() {
 	return (
-		<div className="flex justify-center items-center w-full max-h-56 py-5 bg-black">
-			<div className="flex max-w-7xl">
-				<div>
-					<h2 className="p-2">Links</h2>
-					<div className="lg:flex">
-						<ul className="px-2">
-							<FooterLinkItem link="#">Instagram</FooterLinkItem>
-							<FooterLinkItem link="#">YouTube</FooterLinkItem>
-							<FooterLinkItem link="#">SoundCloud</FooterLinkItem>
-							<FooterLinkItem link="#">TikTok</FooterLinkItem>
+		<div className="flex flex-col items-center w-full pt-5 bg-black">
+			<div className="sm:flex max-w-prose">
+				<div className="pb-5">
+					<h2 className="py-2 px-6 font-serif text-xl">
+						Social Links
+					</h2>
+					<div className="flex">
+						<ul className="px-6 w-1/2">
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="instagram"
+							>
+								Instagram
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="youtube"
+							>
+								YouTube
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="soundcloud"
+							>
+								SoundCloud
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="tiktok"
+							>
+								TikTok
+							</FooterLinkItem>
 						</ul>
-						<ul className="px-2">
-							<FooterLinkItem link="#">Facebook</FooterLinkItem>
-							<FooterLinkItem link="#">GitHub</FooterLinkItem>
-							<FooterLinkItem link="#">Linkedin</FooterLinkItem>
+						<ul className="px-6 w-1/2">
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="facebook"
+							>
+								Facebook
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="github"
+							>
+								GitHub
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="true"
+								icon="linkedin"
+							>
+								Linkedin
+							</FooterLinkItem>
+						</ul>
+					</div>
+				</div>
+				<div className="pb-5">
+					<h2 className="py-2 px-6 font-serif text-xl">Pages</h2>
+					<div className="flex">
+						<ul className="px-6 w-1/2">
+							<FooterLinkItem
+								link="/"
+								redirect="false"
+								icon="home"
+							>
+								Home
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="/photography"
+								redirect="false"
+								icon="camera"
+							>
+								Photography
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="/videos"
+								redirect="false"
+								icon="video"
+							>
+								Videos
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="/music"
+								redirect="false"
+								icon="music"
+							>
+								Music
+							</FooterLinkItem>
+						</ul>
+						<ul className="px-6 w-1/2">
+							<FooterLinkItem
+								link="/projects"
+								redirect="false"
+								icon="laptop"
+							>
+								Projects
+							</FooterLinkItem>
+							<FooterLinkItem
+								link="#"
+								redirect="false"
+								icon="contact"
+							>
+								Contact
+							</FooterLinkItem>
 						</ul>
 					</div>
 				</div>
 			</div>
+			<p className="p-4">joeowen.ca</p>
 		</div>
 	);
 }
 
 type FooterLinkItemProps = {
 	link: string;
+	redirect: string;
+	icon: string;
 	children: string;
 };
 
-function FooterLinkItem({ link, children }: FooterLinkItemProps) {
+function FooterLinkItem({
+	link,
+	redirect,
+	icon,
+	children,
+}: FooterLinkItemProps) {
 	return (
 		<li className="py-0.5">
-			<span
-				className={`icon-${children.toLocaleLowerCase()} pr-1.5`}
-			></span>
-			<a href={link} target="_blank" className="hover:underline">
+			<span className={`icon-${icon} pr-1.5`}></span>
+			<a
+				href={link}
+				target={redirect === 'true' ? '_blank' : ''}
+				className="hover:underline"
+			>
 				{children}
 			</a>
 		</li>
