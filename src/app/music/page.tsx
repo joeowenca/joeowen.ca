027e4e45ed6b27cb9e959d09a22d5947850b3df9
Joe Owen <joe.a.owen@gmail.com>
2023-09-10 12:40:12 -0700

    Add content component to music page

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,20 +1,16 @@
 import Header from '@/components/header';
+import Content from '@/components/content';
 import Footer from '@/components/footer';
 
 export default function Music() {
 	return (
 		<>
-			<Header page="Music" />
-			<main className="flex flex-1 justify-center p-3 mt-24">
-				<div className="flex-1 max-w-prose">
-					<h1 className="text-center font-serif text-2xl">Music</h1>
-					<section className="p-2 pb-3">
-						<pre className="font-sans whitespace-normal">
-							This is the Music page.
-						</pre>
-					</section>
-				</div>
-			</main>
+			<Header page="music" />
+			<Content title="Music">
+				<pre className="font-sans whitespace-normal max-w-prose">
+					This is the music page.
+				</pre>
+			</Content>
 			<Footer />
 		</>
 	);
