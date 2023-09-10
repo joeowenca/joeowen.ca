d7a458de287767193ae7b84c193a1c9b07a0b222
Joe Owen <joe.a.owen@gmail.com>
2023-09-10 14:50:32 -0700

    Create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import Header from '@/components/header';
+import Content from '@/components/content';
+import Footer from '@/components/footer';
+
+export const metadata: Metadata = {
+	title: 'Joe Owen | Contact',
+};
+
+export default function Contact() {
+	return (
+		<>
+			<Header page="contact" />
+			<Content title="Contact">
+				<pre className="font-sans whitespace-normal max-w-prose">
+					My name is Joe, but you can call me anytime.
+				</pre>
+			</Content>
+			<Footer />
+		</>
+	);
+}
