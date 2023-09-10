0f84afe1108944bc60dbb217f00bc8964b8f41b9
Joe Owen <joe.a.owen@gmail.com>
2023-09-10 12:37:02 -0700

    Update content component styling

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -5,10 +5,12 @@ type ContentProps = {
 
 export default function Content({ title, children }: ContentProps) {
 	return (
-		<main className="flex flex-1 justify-center p-3 mt-24">
-			<div className="flex-1 max-w-prose">
-				<h1 className="text-center font-serif text-2xl">{title}</h1>
-				<section className="p-2 pb-3">{children}</section>
+		<main className="flex flex-1 justify-center mt-24">
+			<div className="p-3">
+				<h1 className="pb-5 text-center font-serif text-2xl">
+					{title}
+				</h1>
+				{children}
 			</div>
 		</main>
 	);
