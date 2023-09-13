ed3af3728038e869a80bea67c0257ffbd6925513
Joe Owen <joe.a.owen@gmail.com>
2023-09-12 21:43:48 -0700

    Organize video data in factory method

--- src/app/videos/getYouTubeVideos.ts
+++ src/app/videos/getYouTubeVideos.ts
@@ -3,6 +3,28 @@ const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID;
 const requestURL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=50`;
 
 export default async function GetYouTubeVideos() {
+  const data = await FetchYouTubeVideos();
+
+  return data.items.map((video: any) => {
+    if (video.snippet.description !== "") {
+      return {
+        id: video.id.videoId,
+        title: video.snippet.title,
+        description: video.snippet.description,
+        date: video.snippet.publishedAt,
+        thumbnail: {
+          url: video.snippet.thumbnails.medium.url,
+          width: video.snippet.thumbnails.medium.width,
+          height: video.snippet.thumbnails.medium.height,
+        },
+      };
+    }
+
+    return null;
+  }).filter((video: any) => video !== null);
+}
+
+async function FetchYouTubeVideos() {
   try {
     const response = await fetch(requestURL);
 
@@ -12,7 +34,7 @@ export default async function GetYouTubeVideos() {
 
     return await response.json();
   } catch (error) {
-  console.error(`Error fetching YouTube videos: ${error}`);
-  throw error;
+    console.error(`Error fetching YouTube videos: ${error}`);
+    throw error;
   }
 }
