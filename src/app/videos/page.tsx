9b5392f92199549975fcddcb5784fa5c83a013b6
Joe Owen <joe.a.owen@gmail.com>
2023-09-12 21:44:51 -0700

    Render video data in VideoItem components

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import Header from '@/components/header';
 import Content from '@/components/content';
 import Footer from '@/components/footer';
+import Image from 'next/image';
 
 import GetYouTubeVideos from './getYouTubeVideos';
 
@@ -13,11 +14,12 @@ export const metadata: Metadata = {
 };
 
 export default function Videos() {
-	const [videosList, setVideosList] = useState({});
+	const [videosList, setVideosList] = useState([]);
 
 	useEffect(() => {
 		async function getData() {
-			setVideosList(await GetYouTubeVideos());
+			const data = await GetYouTubeVideos();
+			setVideosList(data);
 		}
 
 		getData();
@@ -27,11 +29,46 @@ export default function Videos() {
 		<>
 			<Header page="videos" />
 			<Content title="Videos">
-				<pre className="font-sans whitespace-normal max-w-prose">
-					This is the videos page.
-				</pre>
+				<div>
+					{videosList.length > 0 ? (
+						videosList.map((video) => (
+							<VideoItem
+								title={video.title}
+								image={video.thumbnail}
+							>
+								{video.description}
+							</VideoItem>
+						))
+					) : (
+						<p>No data available</p>
+					)}
+				</div>
 			</Content>
 			<Footer />
 		</>
+	);
+}
+
+type VideoItemProps = {
+	title: string;
+	image: any;
+	children: string;
+};
+
+function VideoItem({ title, image, children }: VideoItemProps) {
+	return (
+		<section className="flex flex-col lg:flex-row items-center lg:px-2.5 pb-6 max-w-7xl">
+			<Image
+				className="max-w-[65%] lg:max-w-xs 3xl:max-w-sm lg:pb-0 pb-1 lg:pr-5"
+				src={image.url}
+				width={image.width}
+				height={image.height}
+				alt={`${title} thumbnail`}
+			/>
+			<div className="max-w-prose">
+				<h2 className="font-serif text-xl py-3">{title}</h2>
+				<pre className="font-sans whitespace-normal">{children}</pre>
+			</div>
+		</section>
 	);
 }
