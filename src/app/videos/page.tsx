f9fd1bf95ff107ff254b67bdb0d7710a70520fab
Joe Owen <joe.a.owen@gmail.com>
2023-09-16 08:48:42 -0700

    Remove metadata export on client component

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Metadata } from 'next';
 import { useEffect, useState } from 'react';
 import Header from '@/components/header';
 import Content from '@/components/content';
@@ -8,10 +7,6 @@ import Footer from '@/components/footer';
 import Image from 'next/image';
 
 import GetYouTubeVideos from './getYouTubeVideos';
-
-export const metadata: Metadata = {
-	title: 'Joe Owen | Videos',
-};
 
 export default function Videos() {
 	const [videosList, setVideosList] = useState([]);
