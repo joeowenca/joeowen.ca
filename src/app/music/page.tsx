f0f6dbc63dbe95d8b3aa2f7f3bee0b0d6b5ccda9
Joe Owen <joe.a.owen@gmail.com>
2023-09-16 18:43:46 -0700

    Create music item component

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,22 +1,45 @@
-import { Metadata } from 'next';
+'use client';
+
 import Header from '@/components/header';
 import Content from '@/components/content';
 import Footer from '@/components/footer';
-
-export const metadata: Metadata = {
-	title: 'Joe Owen | Music',
-};
+import ReactPlayer from 'react-player';
+import Image, { StaticImageData } from 'next/image';
+import refraction from '../../../public/cover-art/refraction-album-art.jpg';
 
 export default function Music() {
 	return (
 		<>
 			<Header page="music" />
 			<Content title="Music">
-				<pre className="font-sans whitespace-normal max-w-prose">
-					This is the music page.
-				</pre>
+				<div className="flex flex-col">
+					<MusicItem
+						song="https://soundcloud.com/sas-areku/refraction-sas-areku"
+						image={refraction}
+					></MusicItem>
+				</div>
 			</Content>
 			<Footer />
 		</>
+	);
+}
+
+type MusicItemProps = {
+	song: string;
+	image: StaticImageData;
+};
+
+function MusicItem({ song, image }: MusicItemProps) {
+	return (
+		<section className="flex items-center md:w-[46rem] lg:w-[62rem] md:h-56 mb-5">
+			<Image
+				className={'hidden md:block h-56 w-56 mr-5'}
+				src={image}
+				alt={'song cover art'}
+			/>
+			<div className="aspect-square lg:aspect-auto w-full h-full">
+				<ReactPlayer url={song} width="100%" height="100%" />
+			</div>
+		</section>
 	);
 }
