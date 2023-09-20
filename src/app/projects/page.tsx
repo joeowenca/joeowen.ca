455e3adfcbf1d3d09c61fce3d113673fe20c6472
Joe Owen <joe.a.owen@gmail.com>
2023-09-19 20:16:32 -0700

    Update projects manifest with more projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -48,20 +48,26 @@ function ProjectItem({
 	children,
 }: ProjectItemProps) {
 	return (
-		<section className="flex flex-col lg:flex-row items-center pb-6 max-w-7xl">
+		<section className="flex flex-col lg:flex-row items-center pb-6 my-5 max-w-7xl">
 			<Image
 				className="aspect-square object-cover max-w-xxxxs 3xl:max-w-xxxs lg:mb-0 mb-1 lg:mr-5"
 				src={image}
 				alt={`${title} project thumbnail`}
 			/>
-			<div className="max-w-prose grow relative">
+			<div className="max-w-prose">
 				<h2 className="font-serif text-center lg:text-left text-2xl py-3">
 					{title}
 				</h2>
-				<pre className="font-sans whitespace-normal">{children}</pre>
+				<pre className="font-sans whitespace-normal pb-1.5">
+					{children}
+				</pre>
 				<div className="flex flex-col items-center lg:flex-row">
-					<a href={deployment} target="_blank">
-						<button className="icon-play bg-custom-green hover:bg-custom-light-green transition-colors px-4 py-3 lg:mr-5 mb-1.5 mt-3 lg:my-3">
+					<a
+						href={deployment}
+						target="_blank"
+						className={deployment === 'none' ? 'hidden' : ''}
+					>
+						<button className="icon-play bg-custom-green hover:bg-custom-light-green transition-colors px-4 py-3 lg:mr-5 my-1.5 lg:my-3">
 							View deployment
 						</button>
 					</a>
