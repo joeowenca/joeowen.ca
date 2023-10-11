420b29aec8e4c11799d65b4d0b1c332c4105a6fd
Joe Owen <joe.a.owen@gmail.com>
2023-10-10 18:09:57 -0700

    Set up desktop view navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -16,60 +16,30 @@ export default function Navigation({ color }: NavigationProps) {
 
 	return (
 		<div
-			className="items-center w-1/2 ml-auto"
+			className="flex absolute right-0 w-1/2"
 			style={{ color: `rgb(${color}, ${color}, ${color})` }}
-			onClick={() => (showNav ? toggleNav() : '')}
 		>
-			<div
-				className={`lg:hidden icon-nav text-3xl px-3 float-right ${
-					showNav ? 'hidden' : ''
-				}`}
-				onClick={() => toggleNav()}
-			></div>
-			<div
-				className={`lg:flex items-center ${
-					showNav
-						? 'flex transition-opacity opacity-100 bg-custom-blue/95 text-white flex-col justify-center fixed top-0 left-0 w-full h-full'
-						: 'hidden'
-				}`}
-			>
-				<div
-					className={`icon-close text-3xl px-6 absolute top-5 right-0 ${
-						showNav ? '' : 'hidden'
-					}`}
-					onClick={() => toggleNav()}
-				></div>
-				<div
-					className={`flex ${
-						showNav
-							? 'flex-col items-center pb-8'
-							: 'flex-row items-start pb-0 -translate-x-1/2'
-					} `}
-				>
-					<NavLink page="/">Home</NavLink>
-					<NavLink page="/photography">Photography</NavLink>
-					<NavLink page="/videos">Videos</NavLink>
-					<NavLink page="/music">Music</NavLink>
-					<NavLink page="/projects">Projects</NavLink>
-				</div>
-				<div className={`flex ${showNav ? '' : 'absolute right-4'}`}>
-					<SocialLink
-						icon="instagram"
-						link="https://www.instagram.com/joeowen.ca/"
-					/>
-					<SocialLink
-						icon="youtube"
-						link="https://www.youtube.com/@joeowen_mtb"
-					/>
-					<SocialLink
-						icon="soundcloud"
-						link="https://soundcloud.com/sas-areku"
-					/>
-					<SocialLink
-						icon="github"
-						link="https://github.com/Sas-Areku"
-					/>
-				</div>
+			<div className="flex -translate-x-[50%]">
+				<NavLink page="/">Home</NavLink>
+				<NavLink page="/photography">Photography</NavLink>
+				<NavLink page="/videos">Videos</NavLink>
+				<NavLink page="/music">Music</NavLink>
+				<NavLink page="/projects">Projects</NavLink>
+			</div>
+			<div className="absolute right-0 mr-5">
+				<SocialLink
+					icon="instagram"
+					link="https://www.instagram.com/joeowen.ca/"
+				/>
+				<SocialLink
+					icon="youtube"
+					link="https://www.youtube.com/@joeowen_mtb"
+				/>
+				<SocialLink
+					icon="soundcloud"
+					link="https://soundcloud.com/sas-areku"
+				/>
+				<SocialLink icon="github" link="https://github.com/Sas-Areku" />
 			</div>
 		</div>
 	);
@@ -85,7 +55,7 @@ function NavLink({ children, page }: NavLinkProps) {
 
 	useEffect(() => {
 		setActive(window.location.pathname === page);
-	});
+	}, []);
 
 	return (
 		<div
