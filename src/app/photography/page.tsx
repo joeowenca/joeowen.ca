2b6e480ea240cf7b9bb2c847372329cd52177bd8
Joe Owen <joe.a.owen@gmail.com>
2023-10-22 12:11:18 -0700

    Move PhotographyContent into its own component

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,22 +1,10 @@
 import { Metadata } from 'next';
-import Header from '@/components/header';
-import Content from '@/components/content';
-import Footer from '@/components/footer';
+import PhotographyContent from './photography';
 
 export const metadata: Metadata = {
 	title: 'Joe Owen | Photography',
 };
 
 export default function Photography() {
-	return (
-		<>
-			<Header />
-			<Content title="Photography">
-				<pre className="font-sans whitespace-normal max-w-prose mb-[100vh]">
-					Coming soon.
-				</pre>
-			</Content>
-			<Footer />
-		</>
-	);
+	return <PhotographyContent />;
 }
---
+++ src/app/photography/photography.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/header';
+import Content from '@/components/content';
+import Footer from '@/components/footer';
+
+export default function PhotographyContent() {
+	return (
+		<>
+			<Header />
+			<Content title="Photography">
+				<pre className="font-sans whitespace-normal max-w-prose mb-[100vh]">
+					Coming soon.
+				</pre>
+			</Content>
+			<Footer />
+		</>
+	);
+}
