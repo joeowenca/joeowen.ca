cd72387b491dd708a0af8d43ae7e6eadd5715ab8
Joe Owen <joe.a.owen@gmail.com>
2023-10-24 21:23:24 -0700

    Move findCategory scripts into separate file

--- src/app/photography/[...category]/page.tsx
+++ src/app/photography/[...category]/page.tsx
@@ -1,8 +1,5 @@
-import {
-	getPhotographyManifest,
-	CategoryTypes,
-	PhotoTypes,
-} from '../getPhotos';
+import { getPhotographyManifest, CategoryTypes } from '../getPhotos';
+import findCategory from './scripts/findCategory';
 
 type CategoryProps = {
 	params: {
@@ -26,43 +23,4 @@ export default async function Category({ params }: CategoryProps) {
 				: null}
 		</div>
 	);
-}
-
-function findCategory(manifest: CategoryTypes, categoryPath: string) {
-	const categoryFlatList = generateFlatList(manifest);
-	const foundCategory = categoryFlatList.find(
-		(item) => item.path.toLowerCase() === categoryPath,
-	);
-	if (foundCategory) {
-		return foundCategory;
-	} else {
-		console.error('Category not found.');
-		return null;
-	}
-}
-
-function generateFlatList(manifest: CategoryTypes) {
-	const flatList: CategoryTypes[] = [];
-
-	function isCategoryType(
-		item: PhotoTypes | CategoryTypes,
-	): item is CategoryTypes {
-		return item.type === 'Category';
-	}
-
-	function recursiveSearch(
-		manifest: CategoryTypes,
-		flatList: CategoryTypes[],
-	) {
-		manifest.children.map((item) => {
-			if (isCategoryType(item)) {
-				flatList.push(item);
-				recursiveSearch(item, flatList);
-			}
-		});
-	}
-
-	recursiveSearch(manifest, flatList);
-
-	return flatList;
 }
