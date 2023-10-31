72e004e7ad94632f75ff463a2a9d86652f6e8768
Joe Owen <joe.a.owen@gmail.com>
2023-10-30 21:55:47 -0700

    Add text parallax effect

--- src/app/components/showcase.tsx
+++ src/app/components/showcase.tsx
@@ -37,6 +37,7 @@ export default function Showcase() {
 		>
 			<div>
 				<ShowcaseItem
+					id="sports-showcase"
 					title="Sports"
 					image={sportsShowcase}
 					link="/photography"
@@ -44,6 +45,7 @@ export default function Showcase() {
 					{content.showcase.summary.photography.sports}
 				</ShowcaseItem>
 				<ShowcaseItem
+					id="landscapes-showcase"
 					title="Landscapes"
 					image={landscapeShowcase}
 					link="/photography"
@@ -51,6 +53,7 @@ export default function Showcase() {
 					{content.showcase.summary.photography.landscapes}
 				</ShowcaseItem>
 				<ShowcaseItem
+					id="automotive-showcase"
 					title="Automotive"
 					image={automotiveShowcase}
 					link="/photography"
@@ -58,6 +61,7 @@ export default function Showcase() {
 					{content.showcase.summary.photography.automotive}
 				</ShowcaseItem>
 				<ShowcaseItem
+					id="mountain-bike-videos-showcase"
 					title="Mountain biking videos"
 					image={mountainBikingShowcase}
 					link="/videos"
@@ -65,6 +69,7 @@ export default function Showcase() {
 					{content.showcase.summary.videos.mountainbiking}
 				</ShowcaseItem>
 				<ShowcaseItem
+					id="automotive-videos-showcase"
 					title="Automotive videos"
 					image={automotiveVideosShowcase}
 					link="/videos"
@@ -72,6 +77,7 @@ export default function Showcase() {
 					{content.showcase.summary.videos.automotive}
 				</ShowcaseItem>
 				<ShowcaseItem
+					id="skiing-videos-showcase"
 					title="Skiing videos"
 					image={skiingShowcase}
 					link="/videos"
@@ -84,22 +90,47 @@ export default function Showcase() {
 }
 
 type ShowcaseItemProps = {
+	id: string;
 	title: string;
 	image: StaticImageData;
 	link: string;
 	children: string;
 };
 
-function ShowcaseItem({ title, image, link, children }: ShowcaseItemProps) {
+function ShowcaseItem({ id, title, image, link, children }: ShowcaseItemProps) {
+	const [position, setPosition] = useState<number>();
+
+	function handleScroll() {
+		const element = document.getElementById(id);
+
+		if (element) {
+			const elementRect = element.getBoundingClientRect();
+			setPosition((elementRect.top / 10) * -1);
+		} else {
+			console.log('Element not found.');
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
 	return (
-		<section className="relative">
+		<section id={id} className="relative">
 			<Image
 				className="w-[100vw] h-[100vh] aspect-video object-cover"
 				src={image}
 				alt={`${title} showcase image`}
 			/>
 			<div className="absolute top-0 h-full w-full lg:w-1/2 p-3 lg:p-10 flex justify-center lg:items-center black-gradient">
-				<div className="flex flex-col items-center lg:items-start max-w-prose mt-auto lg:mt-0 pb-5 lg:pb-0 z-10">
+				<div
+					style={{ top: position }}
+					className="relative flex flex-col items-center lg:items-start max-w-prose mt-auto lg:mt-0 pb-20 lg:pb-5 lg:pb-0 z-10"
+				>
 					<h2 className="font-serif text-3xl lg:text-4xl py-3">
 						{title}
 					</h2>
