c17ebb29a5fa5791eeef8a3e925e17154a75f0e3
Joe Owen <joe.owen@checkfront.com>
2023-11-26 19:26:22 -0800

    Add margin to the bottom of the content component

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -5,7 +5,7 @@ type ContentProps = {
 
 export default function Content({ title, children }: ContentProps) {
 	return (
-		<main className="flex flex-1 justify-center mt-24">
+		<main className="flex flex-1 justify-center mt-24 mb-16">
 			<div className="p-3">
 				<h1 className="pb-5 text-center font-serif text-2xl">
 					{title}
